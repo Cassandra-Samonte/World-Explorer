93141cf2287ee0417aa9be54e397118130fcc742
Cas <cssamonte@gmail.com>
2023-11-02 12:43:36 -0700

    styled search page

--- part-2/mern-app/frontend/src/components/SearchPage/index.jsx
+++ part-2/mern-app/frontend/src/components/SearchPage/index.jsx
@@ -16,22 +16,21 @@ export default function SearchPage(props) {
         getData(`https://restcountries.com/v3.1/name/${query}`); 
     }
 
-    return (
-        <div>
-            <form onSubmit={handleQuerySubmit}>
-                <label htmlFor="search">
-                    <h1>Search for a Country</h1>
-                </label>
-                <br />
-                <input
-                    name="search"
-                    placeholder="Search for a country..."
-                    value={query}
-                    onChange={event => setQuery(event.target.value)}
-                />
-                <button type="submit">
-                    Search
-                </button>
+
+return (
+        <div className="flex flex-col items-center pt-40">
+            <form onSubmit={handleQuerySubmit} className="p-4">
+                <label htmlFor="search" className="text-gray-700 font-semibold mb-2 flex flex-col items-center">Search for a Country</label>
+                <div className="flex">
+                    <input
+                        name="search"
+                        placeholder="Search for a country..."
+                        value={query}
+                        onChange={event => setQuery(event.target.value)}
+                        className="w-[50vw] border border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:border-blue-500"
+                    />
+                    <button type="submit" className="bg-gray-300 text-gray-700 rounded-md py-2 px-4 ml-2 focus:outline-none">Search</button>
+                </div>
             </form>
 
             <Gallery
@@ -41,5 +40,6 @@ export default function SearchPage(props) {
                 updateDetails={props.setDetailsData}
             />
         </div>
-    );
+  );
 }
+  
