67bd7d0be2047b77ebd0cc5d8c14fc1000784866
Cas <cssamonte@gmail.com>
2023-12-27 14:42:08 -0800

    added header video on home page

--- part-2/mern-app/frontend/src/components/HomePage/index.jsx
+++ part-2/mern-app/frontend/src/components/HomePage/index.jsx
@@ -1,11 +1,22 @@
 import Gallery from '../Gallery'
 import FooterSection from '../FooterSection'
+import myVideo from '../../assets/world.mp4'; 
 import "../../assets/world-logo.png"
 
 
 export default function HomePage(props) {
     return (
         <>
+            {/* Video */}
+            <div className="relative overflow-hidden">
+                <video autoPlay loop playsInline muted className="w-full" style={{ height: '90vh', objectFit: 'cover' }}>
+                    <source src={myVideo} type="video/mp4" />
+                </video>
+                {/* Overlay content */}
+                <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">
+                    <h1 className="text-white text-4xl font-bold">Explore the Countries of the World</h1>
+                </div>
+            </div>
             <Gallery
                 countries={props.countries}
                 updateDetails={props.setDetailsData}
